66002ca3701bc435e6f4c71f22a95c6f23ea6e7d
tao.wang <331871902@qq.com>
2021-12-30 21:42:49 +0800

    feature：88.合并两个有序数组

--- 88.合并两个有序数组.js
+++ 88.合并两个有序数组.js
@@ -1,4 +1,11 @@
 /*
+ * @Author: wangtao
+ * @Date: 2021-12-29 20:51:25
+ * @LastEditors: 汪滔
+ * @LastEditTime: 2021-12-30 21:42:18
+ * @Description: file content
+ */
+/*
  * @lc app=leetcode.cn id=88 lang=javascript
  *
  * [88] 合并两个有序数组
@@ -12,8 +19,24 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
+// var merge = function (nums1, m, nums2, n) {
+//   nums1.splice(m,n,...nums2)
+//   nums1= nums1.sort((a, b) => a - b)
+// }
+var merge = function (nums1, m, nums2, n) {
+  let len1 = m - 1;
+    let len2 = n - 1;
+    let len = m + n - 1;
+    while(len1 >= 0 && len2 >= 0) {
+        // 注意--符号在后面，表示先进行计算再减1，这种缩写缩短了代码
+        nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+    }
+    function arrayCopy(src, srcIndex, dest, destIndex, length) {
+        dest.splice(destIndex, length, ...src.slice(srcIndex, srcIndex + length));
+    }
+    // 表示将nums2数组从下标0位置开始，拷贝到nums1数组中，从下标0位置开始，长度为len2+1
+    arrayCopy(nums2, 0, nums1, 0, len2 + 1);
 
-};
+
+}
 // @lc code=end
-
