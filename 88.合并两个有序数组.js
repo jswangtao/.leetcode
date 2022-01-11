21ee720fc7171d06b26133c2b663f2cc0101e9b4
tao.wang <331871902@qq.com>
2022-01-12 00:17:19 +0800

    feature：88

--- 88.合并两个有序数组.js
+++ 88.合并两个有序数组.js
@@ -2,7 +2,7 @@
  * @Author: wangtao
  * @Date: 2021-12-29 20:51:25
  * @LastEditors: 汪滔
- * @LastEditTime: 2021-12-30 21:42:18
+ * @LastEditTime: 2022-01-12 00:16:47
  * @Description: file content
  */
 /*
@@ -23,20 +23,17 @@
 //   nums1.splice(m,n,...nums2)
 //   nums1= nums1.sort((a, b) => a - b)
 // }
+
 var merge = function (nums1, m, nums2, n) {
   let len1 = m - 1;
-    let len2 = n - 1;
-    let len = m + n - 1;
-    while(len1 >= 0 && len2 >= 0) {
-        // 注意--符号在后面，表示先进行计算再减1，这种缩写缩短了代码
-        nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+  let len2 = n - 1;
+  let len = m + n - 1;
+  while (len2 >= 0) {
+    if (len1 < 0) {
+      nums1[len--] = nums2[len2--];
+      continue;
     }
-    function arrayCopy(src, srcIndex, dest, destIndex, length) {
-        dest.splice(destIndex, length, ...src.slice(srcIndex, srcIndex + length));
-    }
-    // 表示将nums2数组从下标0位置开始，拷贝到nums1数组中，从下标0位置开始，长度为len2+1
-    arrayCopy(nums2, 0, nums1, 0, len2 + 1);
-
-
-}
+    nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+  }
+};
 // @lc code=end
