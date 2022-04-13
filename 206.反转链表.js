42a27e7593c5dc28f22fdd0b88281c3044dc9755
tao.wang <331871902@qq.com>
2022-04-13 09:28:30 +0800

    feature：206.反转链表.js

---
+++ 206.反转链表.js
@@ -0,0 +1,37 @@
+/*
+ * @Author: wangtao
+ * @Date: 2022-04-12 09:15:32
+ * @LastEditors: 汪滔
+ * @LastEditTime: 2022-04-13 09:23:47
+ * @Description: file content
+ */
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+};
+// @lc code=end
